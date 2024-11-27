85d7ecaf62bb7e530faa39ef931bea8e564dcffc
ABUHURAIAH <a.443hurairah@gmail.com>
2024-11-27 14:14:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function openNav(){
+    document.getElementById("ham-box").style.width="20%";
+}
+function closeNav(){
+    document.getElementById("ham-box").style.width="0";
+}
+function openOverlay(){
+    document.getElementById("overlay").style.display="block";
+}
+function closeOverlay(){
+    document.getElementById("overlay").style.display="none";
+}
